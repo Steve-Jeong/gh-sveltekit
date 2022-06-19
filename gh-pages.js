345690cda370b58bd0b0eb0f398b92c7106b0941
SteveJeong <jst0930@gmail.com>
2022-06-19 17:25:02 +0300

    git name rectified

--- gh-pages.js
+++ gh-pages.js
@@ -5,7 +5,7 @@ ghpages.publish(
     branch: 'gh-pages',
     repo: 'https://github.com/Steve-Jeong/gh-sveltekit.git', // Update to point to your repository
     user: {
-      name: 'Steve Jeong', // update to use your name
+      name: 'SteveJeong', // update to use your name
       email: 'jst0930@gmail.com' // Update to use your email
     },
       dotfiles: true
